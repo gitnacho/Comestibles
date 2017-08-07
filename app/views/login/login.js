7d6ffe1c7cc838502576ec9e002dd5abf21bfc53
Nacho Pacheco <nachopacheco@gmail.com>
2017-08-07 16:33:37 -0500

    Conectando a un servidor
    Explica cómo puedes conectar tu aplicación con un servidor

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,9 +1,16 @@
 var frameModule = require("ui/frame");
+var observableModule = require("data/observable");
 var page;
 var email;
 
+var user = new observableModule.fromObject({
+  email: "usuario@dominio.com",
+  password: "contraseña"
+});
+
 exports.loaded = function (args) {
   page = args.object;
+  page.bindingContext = user;
 };
 
 exports.signIn = function () {
